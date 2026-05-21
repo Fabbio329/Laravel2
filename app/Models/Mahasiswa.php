00107c074dd4623bdfcefe09df96239fa2ab30a5
Fabio <fabioaja6@gmail.com>
2026-05-21 23:40:38 +0700

    setup awal

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $fillable = [
+        'prodi_id',
+        'nama',
+        'alamat'
+    ];
+
+    //banyak mahasiswa milik 1 prodi
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class);
+    }
+}
